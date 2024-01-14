e61bf2b5ec167f716a9f7935fc8d07914054e5f5
Attila Szücs <ati.szucs@hotmail.com>
2024-01-14 15:30:21 +0100

    Change html content on navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,12 @@ const router = {
 
     routes: {},
 
-    navigate: function (path, navId) {
+    navigate: async function (path, navId) {
         // Show default page when the selected route does not exist
         const selectedRoute = this.routes[path];
         if (!selectedRoute) {
-            this.navigate('about', 'aboutNav');
+            // TODO: not found page instead?
+            await this.navigate('about', 'aboutNav');
             return;
         }
 
@@ -26,32 +27,51 @@ const router = {
 
         // Change the current route and add the active class to the currently active navlink
         if (path) {
-            this.activeRoute = this.routes[path];
+            this.activeRoute = selectedRoute;
             const nav = document.getElementById(navId);
             if (nav) {
                 nav.classList.add('active');
             }
+
+            const pageUrl = `/pages/${selectedRoute.template}`;
+            const page = await getPage(pageUrl);
+            const contentDiv = document.getElementById('contentContainer');
+            contentDiv.innerHTML = page;
         }
     },
 };
 
-const onPageLoad = () => {
+const getPage = async (url) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            return 'Not found'; // TODO: proper 404 page
+        }
+
+        const page = await response.text();
+        return page;
+    } catch {
+        return 'Failed to load';
+    }
+};
+
+const onPageLoad = async () => {
     // Register routes
-    router.routes['about'] = { template: 'aboutTemplate', path: 'about' };
-    router.routes['projects'] = { template: 'projectsTemplate', path: 'projects' };
-    router.routes['cv'] = { template: 'cvTemplate', path: 'cv' };
-    router.routes['contact'] = { template: 'contactTemplate', path: 'contact' };
+    router.routes['about'] = { template: 'about.html', path: 'about' };
+    router.routes['projects'] = { template: 'projects.html', path: 'projects' };
+    router.routes['cv'] = { template: 'cv.html', path: 'cv' };
+    router.routes['contact'] = { template: 'contact.html', path: 'contact' };
 
     // Preserve active page between page reloads
     if (!router.activeRoute.path) {
         // Try to navigate to the page defined by the URL
         const hash = window.location.href.split('#')[1];
-        router.navigate(hash, hash + 'Nav');
+        await router.navigate(hash, hash + 'Nav');
     }
 }
 
 window.addEventListener('load', onPageLoad);
-window.addEventListener('hashchange', (event) => {
+window.addEventListener('hashchange', async (event) => {
     const hash = event.newURL.split('#')[1];
-    router.navigate(hash, hash + 'Nav');
+    await router.navigate(hash, hash + 'Nav');
 });
